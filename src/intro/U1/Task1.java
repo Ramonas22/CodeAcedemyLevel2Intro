71da6e75594c938db786a4fa77c095014e98d298
Ramonas <sanomaras23@gmail.com>
2022-07-11 18:53:48 +0300

    Third task

---
+++ src/intro/U1/Task1.java
@@ -0,0 +1,14 @@
+package intro.U1;
+
+class Task1 {
+
+    public static void main(String[] args) {
+        print("Vienas");
+        String text = "Du";
+        print(text);
+    }
+
+    static void print(String string){
+        System.out.println(string);
+    }
+}
