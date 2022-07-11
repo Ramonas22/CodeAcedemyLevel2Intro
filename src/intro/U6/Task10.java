63fab113149dd8eb854cf3de3d386b0908fc38ad
Ramonas <sanomaras23@gmail.com>
2022-07-11 19:41:21 +0300

    Eight task

---
+++ src/intro/U6/Task10.java
@@ -0,0 +1,14 @@
+package intro.U6;
+
+import java.util.Scanner;
+
+class Task10 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name");
+
+        System.out.println("Hello " + scanner.nextLine() + "!");
+    }
+}
