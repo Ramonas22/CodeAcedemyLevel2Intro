1a4f920686b565d55e1ff6e938eddcbe105c2422
Ramonas <sanomaras23@gmail.com>
2022-08-11 18:06:29 +0300

    Revision2 tasks + unit tests, need to fix makeOrderException test

---
+++ Revision2/src/Main.java
@@ -0,0 +1,9 @@
+import model.Shop;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println(Shop.importProducts("C:\\Users\\sanom\\Desktop\\Coding Academy\\Level2\\Level2Intro\\Revision2\\products1.txt"));
+    }
+}
